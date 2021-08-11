98f7f66724f82509ca84e9ac1898f1d769749548
pguser <imamuracioli@gmail.com>
2021-08-10 22:06:11 -0300

    Se arregla la imagen de product info

--- app/javascript/components/ProductInfo.jsx
+++ app/javascript/components/ProductInfo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CardMedia from '@material-ui/core/CardMedia';
+import xtype from 'xtypejs';
 
 class ProductInfo extends Component {
+
+  
   constructor() {
     super();
     this.state = { product: {} };
@@ -15,7 +19,10 @@ class ProductInfo extends Component {
           this.setState({product: data});
       })
       .catch(error => console.log('error', error));
+
+
   }
+
 
   handleDelete() {
     fetch(`api/v1/products/${this.props.match.params.id}`, {method: 'DELETE'})
@@ -26,11 +33,48 @@ class ProductInfo extends Component {
   }
 
   render() {
+    let imagen;
+    { console.log("prod")} 
+    const prod = (JSON.parse(JSON.stringify(this.state.product)))
+    const url_image = ''
+    
+
+    if (prod.id !== undefined )
+    {
+      
+       {/*imagen = <img src={JSON.stringify(prod.image_product.url)} alt={prod.name} > </img>  */}     
+      const url_image = JSON.stringify(prod.image_product.url)
+
+      imagen = "http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3f24ce01385eb070514fe1bcf226b7ea4c92de05/Lavarropa.jpg" 
+
+      {/*
+       imagen = <CardMedia
+      image= "http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3f24ce01385eb070514fe1bcf226b7ea4c92de05/Lavarropa.jpg"
+      title={this.state.product.name}
+      />
+      */} 
+
+      { console.log(imagen)} 
+    } else {
+      { console.log("URL vacía")} 
+      imagen = ""
+    }
+   
     return (
       <div>
+        
         { console.log('Estamos en el show')} 
-        <h2>{this.state.product.id}: {this.state.product.name}</h2>
+        {console.log((JSON.parse(JSON.stringify(this.state.product))))}
+        
+        <h2> {this.state.product.name}</h2>
         <p>{this.state.product.price}</p>
+        
+       
+       {/*Esto debe ir sin el cierre </img>, porque arroja un puto error. Solo se cierra usando />*/} 
+        <img src={imagen} alt={this.state.product.name} />
+       
+
+        {/* <img src={this.state.product.image_product.url} alt={this.state.product.name} > </img> */}
         <p>
           <Link to={`/products/${this.state.product.id}/edit`} className="btn btn-outline-dark">Edit</Link> 
           <button onClick={this.handleDelete} className="btn btn-outline-dark">Delete</button> 
