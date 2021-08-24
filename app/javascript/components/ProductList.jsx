b68c31bae9fda8ac64ea9c98769cce77bd6d0f78
ImanolMuracioli <imamuracioli@gmail.com>
2021-08-24 19:53:03 -0300

    Prueba cambio Git en Edit
    Es una prueba de Git

--- app/javascript/components/ProductList.jsx
+++ app/javascript/components/ProductList.jsx
@@ -9,7 +9,6 @@ import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
 import Cardproduct from './Cardproduct';
 
-
 const useStyles = makeStyles((theme) => ({
   gridstylee:{
     paddingTop: '20px',
